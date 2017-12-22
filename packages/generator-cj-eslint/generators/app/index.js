c80d11492fb02249104a2716dfbeed5f46e0ac38
Charlie Jackson <charlie@charliejackson.com>
2017-12-22 14:29:59 +0000

    Install eslint plugins

--- packages/generator-cj-eslint/generators/app/index.js
+++ packages/generator-cj-eslint/generators/app/index.js
@@ -2,6 +2,19 @@
 const Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
+  configuring() {
+    this.yarnInstall([
+      'eslint',
+      'eslint-config-airbnb',
+      'eslint-import-resolver-babel-module',
+      'eslint-plugin-flowtype',
+      'eslint-plugin-import',
+      'eslint-plugin-jest',
+      'eslint-plugin-jsx-a11y',
+      'eslint-plugin-react',
+    ], { 'dev': true });
+  }
+
   writing() {
     this.fs.copy(
       this.templatePath('.*'),
