e2bd8abd4bd5f80c0f94a4ec540a8aad354d3a76
Charlie Jackson <charlie@charliejackson.com>
2017-12-22 19:35:48 +0000

    Removed unnecessary files

--- packages/generator-cj-react-component/generators/app/templates/helpers/helper.js
+++
@@ -1,9 +0,0 @@
-/**
- * Example helper
- *
- * @param  {String} text Text passed to the helper
- * @return {String}      Transformed text
- */
-export default function (text) {
-  return `${text}-value`;
-}
