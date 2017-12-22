0197d4b97431053c9514f7cb7768e6b815373ccf
Charlie Jackson <charlie@charliejackson.com>
2017-12-22 11:54:54 +0000

    Copy the selected files and pass template data

--- packages/generator-cj-react-component/generators/app/index.js
+++ packages/generator-cj-react-component/generators/app/index.js
@@ -7,6 +7,33 @@ module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
     findComponentDir = findComponentDir.bind(this);
+
+    this.fileChoices = [
+      {
+        name: 'Container',
+        checked: true,
+      },
+      {
+        name: 'Component',
+        checked: true,
+      },
+      {
+        name: 'Render',
+        checked: true,
+      },
+      {
+        name: 'Style',
+        checked: true,
+      },
+      {
+        name: 'Helper',
+        checked: false,
+      },
+      {
+        name: 'Tests',
+        checked: false,
+      }
+    ];
   }
 
   initializing() {
@@ -20,6 +47,7 @@ module.exports = class extends Generator {
         name: 'name',
         message: 'Name',
         validate: (value) => value && value.length > 0 || 'Name must be provided',
+        filter: (value) => `${value.charAt(0).toUpperCase()}${value.slice(1)}`,
       },
       {
         type: 'checkbox',
@@ -27,52 +55,105 @@ module.exports = class extends Generator {
         message: 'Files',
         store: true,
         validate: (value) => {
-          if (!value.includes('Container') || !value.includes('Component') || !value.includes('Render')) {
+          if (!value.includes('Container') && !value.includes('Component') && !value.includes('Render')) {
             return 'Container, Component or Render must be selected';
           }
 
           return true;
         },
-        choices: [
-          {
-            name: 'Container',
-            checked: true,
-          },
-          {
-            name: 'Component',
-            checked: true,
-          },
-          {
-            name: 'Render',
-            checked: true,
-          },
-          {
-            name: 'Style',
-            checked: true,
-          },
-          {
-            name: 'Helper',
-            checked: false,
-          },
-          {
-            name: 'Tests',
-            checked: false,
-          }
-        ]
+        choices: this.fileChoices,
       },
     ];
 
     return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
       this.props = props;
-      console.warn(props);
     });
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('*'),
-      join(this.findComponentDir, this.props.name)
+    const componentDir = join(this.componentDir, this.props.name);
+    const templateData = {};
+
+    this.fileChoices.forEach(({ name }) => {
+      templateData[name.toLowerCase()] = this.props.files.includes(name);
+    });
+
+    this.props.files.forEach((file) => {
+      let templatePath;
+      let destinationPath;
+      let type;
+
+      switch(file) {
+        case 'Container':
+        case 'Style':
+          type = file.toLowerCase();
+          templatePath = `Component.${type}.js`;
+          destinationPath = `${this.props.name}.${type}.js`;
+          break;
+
+        case 'Component':
+        case 'Render':
+          type = file.toLowerCase();
+          templatePath = `Component.${type}.jsx`;
+          destinationPath = `${this.props.name}.${type}.jsx`;
+          break;
+
+        case 'Helper':
+          templatePath = 'helpers/helper.js';
+          destinationPath = 'helpers/helper.js';
+          break;
+
+        default:
+          return;
+      }
+
+      this.fs.copyTpl(
+        this.templatePath(templatePath),
+        join(componentDir, destinationPath),
+        templateData
+      );
+    });
+
+    if (this.props.files.includes('Tests')) {
+      this.props.files.forEach((file) => {
+        let templatePath;
+        let destinationPath;
+        let type;
+
+        switch(file) {
+          case 'Container':
+            templatePath = 'Component.container.js';
+            destinationPath = `${this.props.name}.container.js`;
+            break;
+
+          case 'Component':
+          case 'Render':
+            type = file.toLowerCase();
+            templatePath = `Component.${type}.jsx`;
+            destinationPath = `${this.props.name}.${type}.jsx`;
+            break;
+
+          case 'Helper':
+            templatePath = 'helper.js';
+            destinationPath = 'helper.js';
+            break;
+
+          default:
+            return;
+        }
+
+        this.fs.copyTpl(
+          this.templatePath(`__tests__/${templatePath}`),
+          join(componentDir, '__tests__', destinationPath),
+          templateData
+        );
+      });
+    }
+
+    this.fs.copyTpl(
+      this.templatePath('index.js'),
+      join(componentDir, 'index.js'),
+      templateData
     );
   }
 };
