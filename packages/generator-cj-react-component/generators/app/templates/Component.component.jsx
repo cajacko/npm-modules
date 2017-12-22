81cdf4e2cf59ae129a86be95ee16eaf652441be1
Charlie Jackson <charlie@charliejackson.com>
2017-12-22 12:55:10 +0000

    Component template

--- packages/generator-cj-react-component/generators/app/templates/Component.component.jsx
+++ packages/generator-cj-react-component/generators/app/templates/Component.component.jsx
@@ -1,8 +1,22 @@
 import React, { PureComponent } from 'react';
-import Component from 'components/Component/Component.render';
-import help from 'components/Component/helpers/helper';
+import PropTypes from 'prop-types';
+import <%= componentName %> from 'components/<%= componentName %>/<%= componentName %>.render';<% if (helper) { %>
+import help from 'components/<%= componentName %>/helpers/helper';<% } %>
 
-class ComponentComponent extends PureComponent {
+/**
+ * Business logic for the <%= componentName %> component.
+ *
+ * TODO: EXPLAIN CUSTOM BEHAVIOUR HERE
+ *
+ * @extends Component
+ */
+class <%= componentName %>Component extends PureComponent {
+  /**
+   * Initialise the class, set the initial state and bind the methods
+   *
+   * @param  {Object}    props Props passed to the component
+   * @return {undefined}       No return value
+   */
   constructor(props) {
     super(props);
 
@@ -11,36 +25,48 @@ class ComponentComponent extends PureComponent {
     this.state = { value: '' };
   }
 
+  /**
+   * Example method
+   *
+   * @param  {Event}     event Event from the action
+   * @return {undefined}       No return value
+   */
   onChange(event) {
     event.preventDefault();
 
-    const value = helper(event.target.value);
+    const value = <%= helper ? 'helper(' : '' %>event.target.value<%= helper ? ')' : '' %>;
 
     if (value !== this.state.value) this.setState({ value });
     if (this.props.prop3) this.props.prop3(value);
   }
 
+  /**
+   * Render the .render component that handles all the visual presentation.
+   * Passing only the props needed for display.
+   *
+   * @return {String} The html markup to render
+   */
   render() {
     return (
-      <ComponentComponent
+      <<%= componentName %>
         prop1={this.props.prop1}
         prop2={this.props.prop2}
         value={this.state.value}
         onChange=={this.onChange}
       />
-    )
+    );
   }
 }
 
-ComponentComponent.propTypes = {
+<%= componentName %>Component.propTypes = {
   prop1: PropTypes.string.isRequired,
   prop2: PropTypes.bool,
   prop3: PropTypes.func,
 };
 
-ComponentComponent.defaultProps = {
+<%= componentName %>Component.defaultProps = {
   prop2: false,
   prop3: () => {},
 };
 
-export default ComponentComponent;
+export default <%= componentName %>Component;
