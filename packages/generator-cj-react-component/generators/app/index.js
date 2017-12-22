2ff7409410b7d44ea18d2eaa17e0545e4d3d353c
Charlie Jackson <charlie@charliejackson.com>
2017-12-22 11:07:03 +0000

    Added prompts and test files

--- packages/generator-cj-react-component/generators/app/index.js
+++ packages/generator-cj-react-component/generators/app/index.js
@@ -2,26 +2,73 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+let findComponentDir = require('./helpers/findComponentDir');
 
 module.exports = class extends Generator {
-  // prompting() {
-  //   // Have Yeoman greet the user.
-  //   this.log(yosay(
-  //     'Welcome to the groundbreaking ' + chalk.red('generator-cj-react-component') + ' generator!'
-  //   ));
-  //
-  //   const prompts = [{
-  //     type: 'confirm',
-  //     name: 'someAnswer',
-  //     message: 'Would you like to enable this option?',
-  //     default: true
-  //   }];
-  //
-  //   return this.prompt(prompts).then(props => {
-  //     // To access props later use this.props.someAnswer;
-  //     this.props = props;
-  //   });
-  // }
+  constructor(args, opts) {
+    super(args, opts);
+    findComponentDir = findComponentDir.bind(this);
+  }
+
+  initializing() {
+    this.componentDir = findComponentDir();
+  }
+
+  prompting() {
+    const prompts = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name',
+        validate: (value) => value && value.length > 0 || 'Name must be provided',
+      },
+      {
+        type: 'checkbox',
+        name: 'files',
+        message: 'Files',
+        store: true,
+        validate: (value) => {
+          if (!value.includes('Container') || !value.includes('Component') || !value.includes('Render')) {
+            return 'Container, Component or Render must be selected';
+          }
+
+          return true;
+        },
+        choices: [
+          {
+            name: 'Container',
+            checked: true,
+          },
+          {
+            name: 'Component',
+            checked: true,
+          },
+          {
+            name: 'Render',
+            checked: true,
+          },
+          {
+            name: 'Style',
+            checked: true,
+          },
+          {
+            name: 'Helper',
+            checked: false,
+          },
+          {
+            name: 'Tests',
+            checked: false,
+          }
+        ]
+      },
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+      console.warn(props);
+    });
+  }
 
   writing() {
     this.fs.copy(
