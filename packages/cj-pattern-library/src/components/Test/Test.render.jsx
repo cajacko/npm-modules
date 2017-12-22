367e9785301b67d7f87c24618cff024cd1fd0828
Charlie Jackson <charlie@charliejackson.com>
2017-12-22 16:40:50 +0000

    Testing publish changes

--- packages/cj-pattern-library/src/components/Test/Test.render.jsx
+++ packages/cj-pattern-library/src/components/Test/Test.render.jsx
@@ -24,7 +24,7 @@ class Test extends PureComponent {
     return (
       <Container>
         <Div prop1={this.props.prop1}>
-          {this.props.prop2 && <Text>Wee</Text>}
+          {this.props.prop2 && <Text>Groo</Text>}
           <Button onChange={this.props.onChange}>{this.props.value}</Button>
         </Div>
       </Container>
