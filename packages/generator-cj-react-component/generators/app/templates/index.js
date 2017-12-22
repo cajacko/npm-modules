bfe3b91b38e6bc1be66f8b589d691d3a6b6b2be4
Charlie Jackson <charlie@charliejackson.com>
2017-12-22 12:07:37 +0000

    Add todo flag to index comment

--- packages/generator-cj-react-component/generators/app/templates/index.js
+++ packages/generator-cj-react-component/generators/app/templates/index.js
@@ -3,6 +3,8 @@
  * makes it easy to change the component entry path. As many components don't
  * need the full .container, .component and .render files.
  *
+ * TODO: EXPLAIN OVERVIEW OF COMPONENT HERE
+ *
  * As this is an index.js file you only need to specify the import path to the
  * component directory, instead of to index.js.
  */
